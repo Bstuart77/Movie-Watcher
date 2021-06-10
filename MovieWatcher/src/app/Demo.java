069f80c9c00e5f86f6ea9924fca76c0f374e7af0
Brian <bstuart777@gmail.com>
2021-06-09 23:48:12 -0400

    redid file structure

--- MovieWatcher/src/app/Demo.java
+++ MovieWatcher/src/app/Demo.java
@@ -1,19 +1,41 @@
 package app;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Utilities;
 
 public class Demo extends Application{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		Connection conn = null;
+		conn = Utilities.getConnection();
+		try {
+			Statement statement = conn.createStatement();
+			statement.setQueryTimeout(30); // not required, if DB doesnt respond in 30 seconds, it will quit. not
+											// required by good to have
+
+//			Statement s = conn.createStatement();
+//			boolean flag = s.execute("insert into item(name, price) values('Reuben Sandwhich',5.00)");
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			Utilities.closeConnection(conn);
+		}
 		launch(args);
-	}
+
+}
 
 	@Override
 	public void start(Stage pStage) throws Exception {
--- MovieWatcher/src/controller/MainController.java
+++ MovieWatcher/src/controller/MainController.java
@@ -1,22 +1,46 @@
 package controller;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import model.Utilities;
 
 public class MainController implements Initializable{
 @FXML private TextField movieBox;
 @FXML private ComboBox<String> ratingBox;
 
 
-public void insert() {
+public void insert() throws Exception {
 	String movie = movieBox.getText();
 	String rating = ratingBox.getSelectionModel().getSelectedItem().toString();
+	
+	Date date = new Date(); // This object contains the current date value
+	SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	
 	System.out.println(movie + " "  + rating);
+	
+	Connection conn = Utilities.getConnection();
+
+	try {
+	Statement statement = conn.createStatement();
+	
+	statement.execute("INSERT INTO moviesAndShows(movieName, rating, timeWatched)" + "VALUES('" + movie  + rating + date + "')");
+
+	} catch (SQLException e) {
+	// TODO Auto-generated catch block
+		e.printStackTrace();
+	} finally {
+		Utilities.closeConnection(conn);
+	}
 }
 
 
@@ -24,5 +48,5 @@ public void insert() {
 public void initialize(URL arg0, ResourceBundle arg1) {
 	// TODO Auto-generated method stub
 	ratingBox.getItems().addAll("1","2","3","4","5","6","7","8","9","10");
-}
+	}
 }
