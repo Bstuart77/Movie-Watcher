73fc9e5819b63483170751ceedc34240a5184df5
Brian <bstuart777@gmail.com>
2021-06-24 23:58:11 -0400

    class that connects to MySQL database

---
+++ MovieWatcher/src/model/Utilities.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Utilities {
+
+	public static Connection connect() throws ClassNotFoundException, SQLException {
+		Connection conn = null;
+		try {
+			conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/MoviesAndShowsdb", "root","Mydickisitchy12");
+			return conn;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static boolean closeConnection(Connection conn) {
+		if (conn != null) { // there IS a connection
+			try {
+				conn.close();
+				return true;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			return false;
+		}
+		return false;
+	}
+}
