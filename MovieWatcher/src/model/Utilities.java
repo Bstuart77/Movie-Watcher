4a024f1875e6afd4b0f170bc1bf02077661aec53
Brian <bstuart777@gmail.com>
2021-08-15 22:25:52 -0400

    Updated code to read from text files

--- MovieWatcher/src/model/Utilities.java
+++ MovieWatcher/src/model/Utilities.java
@@ -1,21 +1,45 @@
 package model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Utilities {
 
-	public static Connection connect() throws ClassNotFoundException, SQLException {
+	@SuppressWarnings("resource")
+	public static Connection connect() throws ClassNotFoundException, SQLException, FileNotFoundException {
+
+		File file = new File("info/pw.txt");
+		Scanner input = new Scanner(file);
+
+		String pw = input.nextLine();
+
+		file = new File("info/user.txt");
+		input = new Scanner(file);
+
+		String user = input.nextLine();
+
+		file = new File("info/connect.txt");
+		input = new Scanner(file);
+
+		String connection = input.nextLine();
+
 		Connection conn = null;
 		try {
-			conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/moviesandshows", "root","Mydickisitchy12");
+			conn = (Connection) DriverManager.getConnection(connection, user, pw);
 			return conn;
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		input.close();
+
 		return null;
+
 	}
 
 	public static boolean closeConnection(Connection conn) {
